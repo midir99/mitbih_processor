1ed8d47dccf94da26cf65e4eb49a421186f68b5e
jorge4larcon <jorge4larcon@gmail.com>
2021-01-03 14:00:34 -0600

    auto open link feature added

--- mitbih_processor/__main__.py
+++ mitbih_processor/__main__.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
 from webclient import create_app
+import webbrowser
 
 
 def main():
     print('This Python library facilitates the process of extracting and \n'
           'processing ECG signals from MIT-BIH Arrhythmia Database.\n'
           'Launching the web client...')
-
     app = create_app()
-    print(
-        'The app is running on http://127.0.0.1:5000/arrcls/signal_extraction')
+    url = 'http://127.0.0.1:5000/arrcls/signal_extraction'
+    print(f'The app is running on {url}')
     app.run('localhost', 5000)
+    webbrowser.open(url)
 
 
 if __name__ == '__main__':
